6546bc330871f071e5ed41a3bc4a614d6a5e8a1c
abcxyz2008 <huutan1233210@gmail.com>
2025-01-23 15:29:44 +0700

    Pc2.py

--- c2.py
+++ c2.py
@@ -770,21 +770,3 @@ CLEAR   ► CLEAR TERMINAL
             except IndexError:
                 pass
 
-
-def login():
-    clear()
-    user = "2008"
-    passwd = "2008"
-    username = input("⚡ Username: ")
-    password = getpass.getpass(prompt='⚡ Password: ')
-    if username != user or password != passwd:
-        print("")
-        print("⚡ Haizzz, you're so cute...")
-        sys.exit(1)
-    elif username == user and password == passwd:
-        print("⚡ Welcome to ZxC C2!")
-        time.sleep(0.3)
-        ascii_vro()
-        main()
-
-login()
